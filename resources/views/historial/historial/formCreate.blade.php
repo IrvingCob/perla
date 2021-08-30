f91087a9238aaa2ed3fe78dff2e45aee3e8ed298
IrvingCob <irving_cob@hotmail.com>
2021-08-30 14:29:54 -0500

    proyecto completado

--- resources/views/historial/historial/formCreate.blade.php
+++ resources/views/historial/historial/formCreate.blade.php
@@ -5,27 +5,6 @@
         {!! $errors->first('fecha', '<p class="help-block">:message</p>') !!}
 </div>
 </div>
-<div class="form-group {{ $errors->has('cantidad') ? 'has-error' : ''}}">
-    <label for="cantidad" class="col-md-4 control-label">{{ 'Cantidad' }}</label>
-    <div class="col-md-4">
-        <input oninput="calculate()" required class="form-control" name="cantidad" type="text" id="cantidad" value="{{ isset($historial->cantidad) ? $historial->cantidad : ''}}" >
-        {!! $errors->first('cantidad', '<p class="help-block">:message</p>') !!}
-</div>
-</div>
-<div class="form-group {{ $errors->has('precio') ? 'has-error' : ''}}">
-    <label for="precio" class="col-md-4 control-label">{{ 'Precio' }}</label>
-    <div class="col-md-4">
-        <input oninput="calculate()" required class="form-control" name="precio" type="text" id="precio" value="{{ isset($historial->precio) ? $historial->precio : ''}}" >
-        {!! $errors->first('precio', '<p class="help-block">:message</p>') !!}
-</div>
-</div>
-<div class="form-group {{ $errors->has('importe') ? 'has-error' : ''}}">
-    <label for="importe" class="col-md-4 control-label">{{ 'Importe' }}</label>
-    <div class="col-md-4">
-        <input required class="form-control" name="importe" type="text" id="importe" value="{{ isset($historial->importe) ? $historial->importe : ''}}" >
-        {!! $errors->first('importe', '<p class="help-block">:message</p>') !!}
-</div>
-</div>
 {{-- <div class="form-group {{ $errors->has('producto_id') ? 'has-error' : ''}}">
                 @php($producto=App\Models\Producto::pluck('nombre', 'id'))
     <label for="producto_id" class="col-md-4 control-label">{{ 'Producto' }}</label>
@@ -48,6 +27,28 @@
         </select>
     </div>
 </div>
+<div class="form-group {{ $errors->has('cantidad') ? 'has-error' : ''}}">
+    <label for="cantidad" class="col-md-4 control-label">{{ 'Cantidad' }}</label>
+    <div class="col-md-4">
+        <input oninput="calculate()" required class="form-control" name="cantidad" type="text" id="cantidad" value="{{ isset($historial->cantidad) ? $historial->cantidad : ''}}" >
+        {!! $errors->first('cantidad', '<p class="help-block">:message</p>') !!}
+</div>
+</div>
+<div class="form-group {{ $errors->has('precio') ? 'has-error' : ''}}">
+    <label for="precio" class="col-md-4 control-label">{{ 'Precio' }}</label>
+    <div class="col-md-4">
+        <input oninput="calculate()" required class="form-control" name="precio" type="text" id="precio" value="{{ isset($historial->precio) ? $historial->precio : ''}}" >
+        {!! $errors->first('precio', '<p class="help-block">:message</p>') !!}
+</div>
+</div>
+<div class="form-group {{ $errors->has('importe') ? 'has-error' : ''}}">
+    <label for="importe" class="col-md-4 control-label">{{ 'Importe' }}</label>
+    <div class="col-md-4">
+        <input required class="form-control" name="importe" type="text" id="importe" value="{{ isset($historial->importe) ? $historial->importe : ''}}" >
+        {!! $errors->first('importe', '<p class="help-block">:message</p>') !!}
+</div>
+</div>
+
 {{-- 
 <div class="form-group {{ $errors->has('status_id') ? 'has-error' : ''}}">
                 @php($status=App\Models\status::pluck('status', 'id'))
